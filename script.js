a924a5910a3fb6b4dba49ca9a12c2b1c3852a7c3
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-07-30 12:48:45 +0200

    created sort function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const sortButton = document.getElementById("sort");
+const sortInputArray = (event) => {
+    event.preventDefault();
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value)); //array con spread, implicit return senza graffe, Number() per fare il parse in numero 
+};
+
+
+
+sortButton.addEventListener("click", sortInputArray);
+
