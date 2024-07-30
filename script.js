95ecfc7338b542594107fc6472261f7fda17abb0
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-07-30 17:47:13 +0200

    implemented insertion sort function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value)); //array con spread, implicit return senza graffe, Number() per fare il parse in numero 
-    const sortedValues = bubbleSort(inputValues);
+    const sortedValues = insertionSort(inputValues);
     updateUI(sortedValues);
 };
 
@@ -14,6 +14,34 @@ const bubbleSort = (array) => {
                 array[j] = array[j+1];
                 array[j+1] = temp;
             }
+    }
+    return array;
+};
+
+const selectionSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        const temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
+    return array;
+};
+
+const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+        const currValue = array[i];
+        let j = i - 1;
+        while (j >= 0 && array[j] > currValue) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = currValue;
     }
     return array;
 };
