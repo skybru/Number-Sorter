291b9328096a883bfe65dd2bee1d8a7803cc25aa
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-07-30 17:56:46 +0200

    adding sort callback

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value)); //array con spread, implicit return senza graffe, Number() per fare il parse in numero 
-    const sortedValues = insertionSort(inputValues);
+    //const sortedValues = insertionSort(inputValues);
+    const sortedValues = inputValues.sort();
     updateUI(sortedValues);
 };
 
