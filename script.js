1807dd590eb73533cd7090ff4a12127784fe8c46
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-07-30 15:51:44 +0200

    implemented bubblesort function

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value)); //array con spread, implicit return senza graffe, Number() per fare il parse in numero 
+    const sortedValues = bubbleSort(inputValues);
+    updateUI(sortedValues);
+};
+
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++)
+            if (array[j] > array[j+1]) {
+                const temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+    }
+    return array;
 };
 
 const updateUI = (array = []) => {
